964da852c5037358222fa8db39b430b0fe08df5f
issam1994 <issamaitouahmane@gmail.com>
2020-01-21 14:16:03 +0100

    fixed purgecss not working on build for production, now it's working..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import List from './components/list'
 
 function App() {
   return (
     <div className="App">
-      <div class="container bg-gray-200 mx-auto h-screen">
-        <div class="bg-red-500 mx-auto">
-          Hi!
-        </div>
-      </div>
+      <List />
     </div>
   );
 }
