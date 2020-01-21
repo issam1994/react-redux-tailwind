190556f11cf7f5bd9646f03b4a2852deae15b218
issam1994 <issamaitouahmane@gmail.com>
2020-01-21 13:41:04 +0100

    added & configured tailwindcss, postcss, & purgecss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container bg-gray-200 mx-auto h-screen">
+        <div class="bg-red-500 mx-auto">
+          Hi!
+        </div>
+      </div>
     </div>
   );
 }
