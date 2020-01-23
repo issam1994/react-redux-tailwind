4f272384a12a568405456b60c6c0a30a4eb55310
issam1994 <issamaitouahmane@gmail.com>
2020-01-23 15:07:59 +0100

    setting up the playground

---
+++ src/components/shop/Sizes.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Sizes(){
+    return (<div>Sizes</div>)
+}
--- src/views/Shop.jsx
+++ src/views/Shop.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+//importing components
+import Sizes from '../components/shop/Sizes'
+import Product from '../components/shop/Product'
+import Cart from '../components/shop/Cart'
 
-export default function Shop(){
-    return (<div>Shop Root</div>)
+export default function Shop() {
+    return (<div class="container mx-auto pt-16">
+        <Sizes />
+        <Product />
+        <Cart />
+    </div>)
 }
