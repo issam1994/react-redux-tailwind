b26fc941c443be7f7fe0080d2863bb2339b19dbb
issam1994 <issamaitouahmane@gmail.com>
2020-01-27 23:19:14 +0100

    updated the about page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function About() {
     return (
-        <div className="text-center text-gray-700 text-lg">
+        <div className=" max-w-2xl mx-auto text-center text-gray-700 text-lg px-4">
             <h1 className="text-3xl font-semibold">About Page</h1>
             <hr className="mb-4 mt-2 max-w-sm border-blue-600 mx-auto" />
             <p>This React app is for demonstartion purposes.</p>
-            <p>It was built with <b>Redux, React Router, Tailwindcss & React-spring.</b></p>
+            <p>It was built with <b>Redux (state management), React Router (pagination) & React-spring (animations).... & more</b></p>
         </div>
     );
 }
