273bc6858030265159172debea0f90a0ef869409
issam1994 <issamaitouahmane@gmail.com>
2020-01-28 11:23:56 +0100

    resized the increment & decrement button on the cart item, so they stand out more

--- src/components/shop/CartItem.jsx
+++ src/components/shop/CartItem.jsx
@@ -29,8 +29,8 @@ export default function CartItem({ item, index }) {
                 <div className="flex flex-col items-center justify-center ml-auto">
                     <div className="text-yellow-600 text-lg mb-4">$ {price}</div>
                     <div className="text-gray-100 text-sm flex">
-                        <button className="focus:outline-none border-gray-800 w-5 h-5 flex justify-center items-center border bg-gray-800 btn" onClick={()=> dispatch(decrementQuantity(index))} disabled={item.quantity === 1} >-</button>
-                        <button className="focus:outline-none border-gray-800 w-5 h-5 flex justify-center items-center border bg-black btn" onClick={()=> dispatch(incrementQuantity(index))}>+</button>
+                        <button className="focus:outline-none border-gray-800 w-8 h-5 flex justify-center items-center border bg-black btn" onClick={()=> dispatch(decrementQuantity(index))} disabled={item.quantity === 1} >-</button>
+                        <button className="focus:outline-none border-gray-800 w-8 h-5 flex justify-center items-center border bg-black btn" onClick={()=> dispatch(incrementQuantity(index))}>+</button>
                     </div>
                 </div>
             </div>
