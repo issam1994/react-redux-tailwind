c7060af141740f0e27b22beee33896c433c7dfe5
issam1994 <issamaitouahmane@gmail.com>
2020-01-28 19:45:32 +0100

    for cart items, I adjusted the text size responsiveness for  small screens

--- src/components/shop/CartItem.jsx
+++ src/components/shop/CartItem.jsx
@@ -16,14 +16,14 @@ export default function CartItem({ item, index }) {
             <div className="absolute z-10 mx-6 inset-x-0 top-0 text-white border-t border-black flex"><button onMouseEnter={() => setHoveredOnDelete(true)} onMouseLeave={() => setHoveredOnDelete(false)} onClick={() => dispatch(removeFromCart(id))} className="ml-auto self-start px-1 font-bold text-black hover:text-white">X</button></div>
             <div className="flex items-center pb-3 px-6 py-4" style={hoveredOnDelete ? hoveredOnDeleteStyle : null}>
                 {/* item image */}
-                <div className=" h-20 bg-yellow-600">
+                <div className="flex-none h-20 bg-yellow-600">
                     <img className="h-full" src={image} alt={title} />
                 </div>
                 {/* item details */}
-                <div className="text-gray-700 px-4 text-lg">
-                    <div className="text-white text-base font-semibold">{title}</div>
-                    <div className="text-sm">{style}</div>
-                    <div className="text-base">Quantity: <span>{item.quantity}</span></div>
+                <div className="text-gray-700 px-4">
+                    <div className="text-white text-sm sm:text-base font-semibold">{title}</div>
+                    <div className="text-xs sm:text-sm">{style}</div>
+                    <div className="text-sm sm:text-base">Quantity: <span>{item.quantity}</span></div>
                 </div>
                 {/* item price & controls*/}
                 <div className="flex flex-col items-center justify-center ml-auto">
