a9d9cc6b1d240769c752e82c10f738f02f6eae3b
issam1994 <issamaitouahmane@gmail.com>
2020-02-01 17:24:48 +0100

    changed the source of photos that feed the masonry component

--- src/components/masonry/Masonry.jsx
+++ src/components/masonry/Masonry.jsx
@@ -1,16 +1,13 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Masonry() {
     const [images, setImages] = useState([])
     useEffect(() => {
         const fetchImages = async () => {
-            console.log("fetching..")
             try {
-                console.log("fetching images...")
-                let res = await fetch('http://www.splashbase.co/api/v1/images/search?query=laptop');
-                let data = await res.json()
-                setImages(data.images)
-            } catch(e) {
+                let terms = ['nature', 'girl', 'mountains', 'laugh', 'beautiful', 'handsome', 'happy', 'cat', 'sea', 'beach', 'bike', 'car', 'dog', 'birds', 'tech', 'art', 'science', 'redhead,girl', 'desert,sun', 'space', 'camel', "dog,cat", "fish", "food", "street", "music", "night", "sport", "marriage", 'vacation']
+                setImages(terms.map(e => ({src: "https://source.unsplash.com/800x900/?" + e, query: e})))
+            } catch (e) {
                 console.log("failed to fetch images on mounted..", e);
             }
         }
@@ -18,8 +15,8 @@ export default function Masonry() {
     }, []);
     return (
         <div className="px-2 column-count-1 sm:column-count-2 md:column-count-3 lg:column-count-4 transition">
-            {images.map((image, i) => (<div key={i} className="border transition hover:scale" style={i === 5 ? {height: '400px'} : {}}>
-                <img className="object-cover h-full hover:shadow-2xl" src={image.url} alt={image.url} />
+            {images.map((image, i) => (<div key={i} className="my-2 border transition hover:scale" style={i > 2 && i < 8 ? { height: `${i}00px` } : {}}>
+                <img className="object-cover object-center h-full hover:shadow-2xl" src={image.src} alt={image.query} />
             </div>))}
         </div>
     )
